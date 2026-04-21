3362d79b878149dfe8d4c16a4e6a85e094398f0a
Elvis Ugboaja <elvisugboaja85@gmail.com>
2026-04-21 07:19:48 -0700

    pythonassignment

---
+++ pythonAssig.py
@@ -0,0 +1,18 @@
+noun1 = "local"
+adjective = "tired"
+adjective2 = "fun"
+thing = "umbrella"
+weather1 = "cloudy"
+weather2 = "rain"
+
+print (f"Last week was {adjective2}ny")
+print (f"I went to the {noun1} market")
+print (f"I was supposed to use an {thing}, but I did not")
+print(f"My sister had told me to take an {thing} to the {noun1} market")
+print(f"But I was so {adjective} to take an {thing} to the {noun1} market")
+print ("    ")
+print (f"It was {weather1}, but I believed it would not {weather2}")
+print(f"Funny enough, the {weather2} started falling immediately I stepped out of the {noun1} market")
+print ("   ")
+print(f"Well, your guess is as good as mine, the {weather2} had {adjective2} dealing wih me")
+
